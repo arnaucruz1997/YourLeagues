c66f3ea7f06a89b01441158a76ff2cd7e532c489
arnaucruz1997 <arnau.cruz.gargallo@gmail.com>
2022-05-04 02:44:34 +0200

    jugador home-page list teams and competitions

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
+import { Jugador, Organitzador, User } from 'src/app/models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
+import { TeamService } from 'src/app/services/team.service';
+import { Equip } from 'src/app/models/equip';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -10,10 +12,14 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class HomePageComponent implements OnInit {
   usuari:User;
+  jugador:Jugador;
+  organitzador:Organitzador;
+  equips:Equip[];
   constructor(
     public authService:AuthService, 
     public userService:UserService,
     public route:ActivatedRoute,
+    public teamService:TeamService,
     ) { 
 
   }
@@ -21,6 +27,22 @@ export class HomePageComponent implements OnInit {
     this.route.data.subscribe(
       data => {
         this.usuari = data['user'][0];
+        if(this.usuari.rol == "Jugador"){
+          this.jugador = data['user'][0];
+          if(this.jugador.equips.length>0){
+            this.teamService.getUserTeams(this.jugador.equips).subscribe(
+              data=>{
+                console.log("data: ",data);
+                this.equips = data;
+              }
+            );
+          }else{
+            this.equips=[];
+          }
+        }else{
+          this.organitzador = data['user'][0];
+        }
+
       }
     )
   }
